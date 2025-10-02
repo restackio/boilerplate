50f998430b654e6ac2ea185fedbd8ccc5e45f3f6
Osman Hernandez <osman.sist@gmail.com>
2025-10-02 18:28:23 +0200

    ruff fix

--- apps/backend/src/database/models.py
+++ apps/backend/src/database/models.py
@@ -11,7 +11,7 @@ from sqlalchemy import (
     Integer,
     String,
     Text,
-    UniqueConstraint
+    UniqueConstraint,
 )
 from sqlalchemy.dialects.postgresql import JSONB, UUID
 from sqlalchemy.ext.declarative import declarative_base
